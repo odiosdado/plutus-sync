76cac362c93ac1948f47f9a4c6236be389f2907a
Oscar Diosdado <oscar@ghs-limited.com>
2017-11-26 09:26:56 -0600

    Added endpoints for retrieving top plutusScore and also by range

--- src/main/java/com/oddevs/plutus/service/PlutusScoreImpl.java
+++ src/main/java/com/oddevs/plutus/service/PlutusScoreImpl.java
@@ -1,5 +1,6 @@
 package com.oddevs.plutus.service;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -17,9 +18,28 @@ public class PlutusScoreImpl implements PlutusScoreService {
 	private EntityManager em;
 
 	@Override
-	public List<StockHistory> findTopByRange(int range) {
-		TypedQuery<StockHistory> query = em.createQuery("select a from Account a where a.customer = ?1", StockHistory.class);
-		query.setParameter(1, range);
+	public List<StockHistory> findTop(int range) {
+		TypedQuery<StockHistory> query = 
+				em.createQuery("select a from StockHistory a "
+						+ "where a.id in (select max(b.id) "
+						+ "from StockHistory b "
+						+ "group by b.stock) "
+						+ "order by a.plutusScore desc)", StockHistory.class);
+		
+		query.setMaxResults(range);
+		return query.getResultList();
+	}
+	
+	@Override
+	public List<StockHistory> findByRange(BigDecimal begin, BigDecimal end) {
+		TypedQuery<StockHistory> query = 
+				em.createQuery("select a from StockHistory a "
+						+ "where a.id in (select max(b.id) "
+						+ "from StockHistory b "
+						+ "group by b.stock) "
+						+ "and a.plutusScore >= ?1 and a.plutusScore <= ?2 )", StockHistory.class);
+		query.setParameter(1, begin);
+		query.setParameter(2, end);
 		return query.getResultList();
 	}
 }
--- src/main/java/com/oddevs/plutus/service/PlutusScoreService.java
+++ src/main/java/com/oddevs/plutus/service/PlutusScoreService.java
@@ -1,11 +1,13 @@
 package com.oddevs.plutus.service;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import com.oddevs.plutus.domain.StockHistory;
 
 public interface PlutusScoreService {
 
-	List<StockHistory> findTopByRange(int range);
+	List<StockHistory> findTop(int top);
+	List<StockHistory> findByRange(BigDecimal begin, BigDecimal end);
 	
 }
