3346aaacdf63ae649566d1144b115d74436b78ef
Oscar Diosdado <oscar@ghs-limited.com>
2020-04-22 20:27:46 -0500

    Added start.sh to container

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# wait for istio network setup to be complete before starting
 trap "curl --max-time 2 -s -f -XPOST http://127.0.0.1:15020/quitquitquit" EXIT
 while ! curl -s -f http://127.0.0.1:15020/healthz/ready; do sleep 1; done
 echo "Ready!"
